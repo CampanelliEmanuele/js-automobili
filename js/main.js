2aae22f0eea6efe4a94962ca8670e3b839ae38f7
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2023-11-22 18:18:38 +0100

    Refactor: object to handle carKeys

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
-/* Constants declaration */
-const brandKey = "brand", modelKey = "model", fuelKey = "fuel"
+/** Constants declaration */
+const carKeys = {
+    brandKey: "brand",
+    modelKey: "model",
+    fuelKey: "fuel"
+}
+
 const numOfBrands = 5, numOfModels = 10, numOfFuels = 5
 
-/* 1 - Array of cars creation */
+/** 1 - Array of cars creation */
 cars = getArr(10, createCar, "brand", "model", "fuel")
 
-/* 2 - Array filtering */
-let petrolCars = sortArr(cars, fuelKey, "fuel 1")
-let diselCars = sortArr(cars, fuelKey, "fuel 2")
-let otherCars = sortArr(cars, fuelKey, "fuel 3", "fuel 4", "fuel 5")
+/** 2 - Array filtering */
+let petrolCars = sortArr(cars, carKeys.fuelKey, "fuel 1")
+let diselCars = sortArr(cars, carKeys.fuelKey, "fuel 2")
+let otherCars = sortArr(cars, carKeys.fuelKey, "fuel 3", "fuel 4", "fuel 5")
 
-printCars()
+// printCars()
+
 
 /**
  * 1 - Functions
  */
 function createCar(brand, model, fuel) {
     const car = {
-        [brandKey]: brand + ' ' + Math.floor(Math.random() * numOfBrands + 1),
-        [modelKey]: model + ' ' + Math.floor(Math.random() * numOfModels + 1),
-        [fuelKey]: fuel + ' ' + Math.floor(Math.random() * numOfFuels + 1)
+        [carKeys.brandKey]: brand + ' ' + Math.floor(Math.random() * numOfBrands + 1),
+        [carKeys.modelKey]: model + ' ' + Math.floor(Math.random() * numOfModels + 1),
+        [carKeys.fuelKey]: fuel + ' ' + Math.floor(Math.random() * numOfFuels + 1)
     }
     return car;
 }
@@ -32,7 +38,6 @@ function getArr(length, func, ...args) {
     }
     return arr
 }
-
 
 /**
  * 2 - Functions
