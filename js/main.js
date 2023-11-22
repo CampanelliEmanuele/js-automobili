52de93fe00683cad033b3bfe4a5cc382efb7d8d8
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-22 15:34:11 +0100

    customizing array creation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 
 function createCar(brand, model, fuel) {
-    // console.log(`${brand}\t${model}\t${fuel}`)
     let car = {
         brand: brand,
         model: model,
@@ -10,30 +9,18 @@ function createCar(brand, model, fuel) {
     return car;
 }
 
-function getArgs(args) {
-    let output = ''
-    // args.forEach(function(element) {
-    //     output += "\"" + element + "\""
-    //     console.log(output)
-    // })
-
-    return output
-}
-
 function getArr(length, func, ...args) {
-    args = getArgs(args)
     let arr = []
     for (let i = 0; i < length; i++) {
-        let newEl = func(args)
+        let newEl = func.apply(null, args)
+        console.log(newEl)
         arr.push(newEl)
     }
-
     return arr
 }
 
-let args = ["brand", "model", "fuel"]
-arr = getArr(5, createCar, args)
-
+arr = getArr(5, createCar, "brand", "model", "fuel")
+console.log(arr)
 
 
 
